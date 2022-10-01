d25637042c211029e8d7d5f42aeb25bd9ec5dc98
valkyrie_pilot <valk@randomairborne.dev>
2022-09-30 18:03:48 -0600

    stuff n things

--- src/handle.rs
+++ src/handle.rs
@@ -7,7 +7,7 @@ use twilight_model::{
 use worker::{Request, Response};
 
 pub async fn handle(req: Request, env: worker::Env, data: Interaction) -> worker::Result<Response> {
-    if let Ok(kv) = env.kv("jolteon") {
+    if let Ok(kv) = env.kv("tags") {
         match data.kind {
             InteractionType::Ping => Response::from_json(&json!({ "kind": 1 })),
             InteractionType::ApplicationCommand => handle_command(req, kv, data).await,
